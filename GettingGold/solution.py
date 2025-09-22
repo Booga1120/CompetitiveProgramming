a4a63cfc27cd0674a2c35a99d4d6ff929acac3f4
Booga1120 <59713845+Booga1120@users.noreply.github.com>
2025-09-22 11:57:37 -0400

    update

---
+++ GettingGold/solution.py
@@ -0,0 +1,28 @@
+import sys
+from collections import defaultdict, deque, Counter
+from heapq import heappush, heappop
+import math
+
+def main():
+    # Standard input reading
+    # input = sys.stdin.read
+    # data = input().splitlines()
+
+    # Process a line at a time
+    # input = sys.stdin.readline
+    
+    # Multiple integers on one line
+    # a, b = map(int, input().split())
+    
+    # Read until EOF
+    # for line in sys.stdin:
+    # or just normal read line and then say if not line:  # EOF reached
+    #        break
+
+    
+    # YOUR CODE HERE
+    
+    pass
+
+if __name__ == "__main__":
+    main()

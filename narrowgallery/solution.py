ca7a374f16ac6a2d07a149e177297e048fe7cd0f
BoogaCY <cyangrh2025@gmail.com>
2025-10-02 04:05:18 -0400

    update

--- narrowgallery/solution.py
+++ narrowgallery/solution.py
@@ -1,96 +1,64 @@
 import sys
-from collections import defaultdict, deque, Counter
-from heapq import heappush, heappop
-import math
 
 def main():
-    sys.setrecursionlimit(100000)
-    n,k = map(int, sys.stdin.readline().split())
-
-    lefthall = []
-    righthall = []
-    leftsum = 0
-    rightsum = 0
-
-    for i in range(k):
-        a,b = map(int, sys.stdin.readline().split())
-        lefthall.append(a)
-        righthall.append(b)
-        leftsum+=a
-        rightsum+=b
-
-    print(lefthall)
-    print(righthall)
-    print(leftsum)
-    print(rightsum)
-    
-    if(leftsum > rightsum):
-        solutions = [1]*k
-    else:
-        solutions = [3]*k
-
-    def calc(path):
-        sum = 0
-        idx = 0
-        for x in path:
-            if x == 3:
-                sum += righthall[idx]
-            if x == 2:
-                sum += righthall[idx]+lefthall[idx]
-            if x == 1:
-                sum += lefthall[idx]
-            idx+=1
-        return sum
-    
-    def swap(path,spot):
-        idx = 0
-        path[spot]=2
-        for x in path:
-            if idx > spot:
-                path[idx]=4-idx
-            idx+=1
-    
-    bonus = 0
-    for i in range(n-k):
-        a,b = map(int, sys.stdin.readline().split())
-        lefthall.append(a)
-        righthall.append(b)
-        if(solutions[-1]==3):
-            solutions.append(3)
-        else:
-            solutions.append(1)
-        idx = 0
-        bestPathValue = calc(solutions) #default to value if there was no swap
-        didNotSwap = True
-        for idx in range(len(solutions)):
-            if(didNotSwap and solutions[idx] != 2):
-                potentialSolution = swap(solutions, idx)
-                potentialNewValue = calc(potentialSolution)
-                if (potentialNewValue>=bestPathValue):
-                    bestPathValue = potentialNewValue
-                    solutions = potentialSolution
-                    didNotSwap = False
-                    print(idx)
-        if(didNotSwap):
-            bonus+=1
-
-    unused = []
-    result = 0
-    for i in range(n):#find all unused grids, find current path value
-        if(solutions[i]==1):
-            unused.heappush(-righthall[i])
-            result+=lefthall[i]
-        elif(solutions[i]==3):
-            unused.heappush(-lefthall[i])
-            result+=righthall[i]
-        else:
-            result+=lefthall[i]+righthall[i]
-    for i in range(bonus):
-        result+=-heappop(unused)
-
-
-    
-
+    while True:
+        line = sys.stdin.readline().split()
+        n, k = int(line[0]), int(line[1])
+        
+        if n == 0 and k == 0:
+            break
+        
+        lefthall = []
+        righthall = []
+        totalsum = 0
+        
+        for i in range(n):
+            a, b = map(int, sys.stdin.readline().split())
+            lefthall.append(a)
+            righthall.append(b)
+            totalsum += a + b
+        
+        if k == 0:
+            print(totalsum)
+            continue
+        
+        INF = float('inf')
+        dp = [[[INF] * 3 for _ in range(k + 1)] for _ in range(n)]
+        
+        dp[0][0][2] = 0
+        dp[0][1][0] = lefthall[0]
+        dp[0][1][1] = righthall[0]
+        
+        for row in range(1, n):
+            for closed in range(k + 1):
+                left_val = lefthall[row]
+                right_val = righthall[row]
+                
+                if dp[row-1][closed][0] < INF:
+                    dp[row][closed][2] = min(dp[row][closed][2], dp[row-1][closed][0])
+                if dp[row-1][closed][1] < INF:
+                    dp[row][closed][2] = min(dp[row][closed][2], dp[row-1][closed][1])
+                if dp[row-1][closed][2] < INF:
+                    dp[row][closed][2] = min(dp[row][closed][2], dp[row-1][closed][2])
+                
+                if closed > 0:
+                    if dp[row-1][closed-1][0] < INF:
+                        dp[row][closed][0] = min(dp[row][closed][0], 
+                                                dp[row-1][closed-1][0] + left_val)
+                    if dp[row-1][closed-1][2] < INF:
+                        dp[row][closed][0] = min(dp[row][closed][0], 
+                                                dp[row-1][closed-1][2] + left_val)
+                    
+                    if dp[row-1][closed-1][1] < INF:
+                        dp[row][closed][1] = min(dp[row][closed][1], 
+                                                dp[row-1][closed-1][1] + right_val)
+                    if dp[row-1][closed-1][2] < INF:
+                        dp[row][closed][1] = min(dp[row][closed][1], 
+                                                dp[row-1][closed-1][2] + right_val)
+        
+        min_sacrifice = min(dp[n-1][k][0], dp[n-1][k][1], dp[n-1][k][2])
+        result = totalsum - min_sacrifice
+        print(result)
 
 if __name__ == "__main__":
     main()
